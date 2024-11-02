0c4385297286ce33255f2a6c240564597d4c460e
AlexandersWrld <alexanderboydjm@gmail.com>
2024-11-02 13:47:55 -0400

    updating menu command to show different operations, forgot to before

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -4,4 +4,6 @@ from app.commands import Command
 
 class MenuCommand(Command):
     def execute(self):
-        print(f'Menu')
+        print(f'Welcome to the Calulator App')
+        print(f'Simply enter add, subtract, divide or multiply to begin')
+        print(f'Past calculattions may be viewed by entering: history')
